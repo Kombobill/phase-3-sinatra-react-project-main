601672579e0ce602d82a4fb30191253a5cf6f0f0
Kombobill <118278282+Kombobill@users.noreply.github.com>
2023-03-07 11:46:27 +0300

    recreate

--- db/migrate/20220325224143_create_todos.rb
+++
@@ -1,9 +0,0 @@
-class CreateTodos < ActiveRecord::Migration[6.1]
-  def change
-    create_table :todos do |t|
-      t.string :title
-      t.integer :todo_category_id
-      t.timestamps
-    end
-  end
-end
